ed8e6208dfdd3cfd54d53f3a9933defccf13613f
leekoby <118284808+leekoby@users.noreply.github.com>
2022-12-23 19:48:29 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,40 @@
+function solution(array) {
+    let sortedArray = array.sort((a,b)=>a-b);
+    var answer = 0;
+    
+    let choi = -1; //최빈값이 뭔지
+    let choiRepeatCnt=0; // 최빈값이 될때 몇번 반복해서 된건지 
+    let repeatCnt =0; // 현재 똑같은 숫자가 몇번 등장했는지
+    let beforeNumber = -1; // 지금 보고 있는 숫자 이전 숫자 
+    let isDupChoi=false;
+  	
+    
+    let cnt = 0;           
+    while ( cnt< array.length){
+        if(beforeNumber != array[cnt]){
+            repeatCnt = 1;
+        }else {
+            repeatCnt ++;
+        }
+        if(repeatCnt ===choiRepeatCnt){
+            if(choi !== array[cnt]){
+            	isDupChoi = true;
+            }
+        }
+        
+        if(repeatCnt>choiRepeatCnt){
+            choi=array[cnt];
+            choiRepeatCnt = repeatCnt;
+            isDupChoi=false
+        }
+        beforeNumber=array[cnt];
+        
+     cnt ++;
+    }
+    
+   
+    
+    if(isDupChoi) return -1;
+    return choi;
+    
+}
