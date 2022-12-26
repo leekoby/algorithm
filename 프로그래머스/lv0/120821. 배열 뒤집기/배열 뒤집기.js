4c53abd2ec710b84f05c27acb5c1342d20b74486
leekoby <118284808+leekoby@users.noreply.github.com>
2022-12-27 05:37:52 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let newArray = [];
+    let cnt = num_list.length - 1;
+    while (cnt >=0)
+    {   
+        newArray.push(num_list[cnt]);
+        cnt--
+    }
+    return newArray;
+}
