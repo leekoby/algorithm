f6832a752a618b620f7f5f90f95c40f6027f7807
leekoby <118284808+leekoby@users.noreply.github.com>
2022-12-27 05:13:31 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < numbers.length ; i ++){
+        
+        sum += numbers[i];
+    }
+    answer = sum / numbers.length
+    return answer;
+}
