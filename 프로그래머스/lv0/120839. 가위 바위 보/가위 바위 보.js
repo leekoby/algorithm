0974b7792fb2489d8b3bcc13ab1205a98807717a
leekoby <118284808+leekoby@users.noreply.github.com>
2022-12-31 22:24:04 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,19 +1,17 @@
+const win={
+    '2':'0',
+    '0':'5',
+    '5':'2',
+}
+
 function solution(rsp) {
     var answer = '';
-    let winrsp = '';
+
     for (i=0;i<rsp.length;i++)
     {
-        if(rsp[i] ==='2'){
-           winrsp += 0;}
-        else if(rsp[i] ==='0'){
-            winrsp += 5;}
-        else if(rsp[i] ==='5'){
-            winrsp += 2;
-        }
-        
-        
-        
+       const item = rsp[i];
+        answer += win[item];
     }
     
-    return winrsp;
+    return answer;
 }
