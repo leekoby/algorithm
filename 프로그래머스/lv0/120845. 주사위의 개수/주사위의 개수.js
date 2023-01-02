61fa1bed8ad0e3a5c0b1fe2e0cb1cbf24b42dbf7
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-02 21:48:20 +0900

    [level 0] Title: 주사위의 개수, Time: 2.33 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -0,0 +1,12 @@
+function solution(box, n) {
+    var answer = 0;
+    n1=parseInt(box[0]/n)
+    
+    n2=parseInt(box[1]/n)
+    
+    n3=parseInt(box[2]/n)
+    console.log(n1)
+    console.log(n2)
+    console.log(n3)
+    return n1*n2*n3;
+}
