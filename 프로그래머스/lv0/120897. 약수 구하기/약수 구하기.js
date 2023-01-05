45c8c1f57444a4d210f632c85582d7cb31f7367f
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-05 20:55:11 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = [];
+    
+    for (let i = 1 ; i <=n ; i ++ )
+    {
+        if(n%i === 0){
+            answer.push(i);
+        }
+    }    
+    
+    
+    
+    
+    return answer.sort((a,b)=>a-b);
+}
