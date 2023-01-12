836e81256cbfb420e7abfc021109236f76094533
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-12 21:36:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,7 +1,5 @@
 function solution(x) {
     let str = String(x)
-    let arr = str.split('')
-    let h = arr.reduce((a,b)=>+a+ +(+b), 0)
-    console.log(x , h , x%h)
+    let h = str.split('').reduce((a,b)=>+a+ +(+b), 0)
     return x % h === 0 
 }
