beca9da1ac6ccb52803951651191468fe8bc2d6f
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 23:04:01 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,14 +1,6 @@
 function solution(absolutes, signs) {
-    var answer = 0;
-    for (let i = 0 ; i < absolutes.length; i++)
-        {
-            if(signs[i]===true){
-                answer += absolutes[i]
-            }else answer -= absolutes[i]
+    
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
             
-            
-            
-            
-        }
-    return answer;
+       
 }
