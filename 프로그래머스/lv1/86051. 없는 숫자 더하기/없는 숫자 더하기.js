5feef847c002047b2d689bde65bb2c07bdf5a4b4
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 23:10:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,3 @@
 function solution(numbers) {
-    var answer = 0;
-    
-    for(let i =0 ; i<= 9; i ++ ){
-        numbers.includes(i)? console.log(i) : answer+=i
-        
-    }
-    return answer;
+   return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
 }
