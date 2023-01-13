faa2324e0b4c1256284c916675aac55ca143168a
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 09:15:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,21 +1,10 @@
 function solution(num) {
-    var answer = false;
-    let cnt = 0;
-    if(num === 1) return 0
-    while( cnt < 500 ){
-        
-        if(num%2===0)
-            {num =num/2}    
-        else if(num%2===1)
-            {num = num*3+1}        
-            
-        cnt++
-        
-        if(num === 1){return cnt }
-        }
-    
-    
-    return -1;
+    var answer = 0;
+    while(num!==1 && answer !== 500 ){        
+        num%2 === 0 ? num = num/2 : num = num*3+1         
+        answer++;
+    }      
+    return num === 1 ? answer : -1;
     
     
 }
