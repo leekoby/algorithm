6ed3ac665071d02e541c21966dfeec7f92d08b18
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 23:35:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,10 +1,5 @@
 function solution(s) {
-    var answer = [];
-    let arr = s.split(' ')
-    console.log(arr)
-    answer.push(Math.min(...arr))
-    console.log(arr)
-    answer.push(Math.max(...arr))
-    console.log(arr)
-    return answer.join(' ');
+    const arr = s.split(' ');
+
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
