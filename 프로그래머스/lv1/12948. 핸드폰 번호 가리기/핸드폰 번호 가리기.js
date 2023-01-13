522960da9f7ee3568438bf08219a01827f45bbe3
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 09:43:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,9 @@
 function solution(phone_number) {
-    let str = phone_number.split('')
-    var answer = [];
-    for(let i = 0; i < str.length ; i++ ){
-        i > str.length-5 ? answer.push(str[i]) : answer.push('*')          
-    }    
-    return answer.join('');
+    // let str = phone_number.split('')
+    // var answer = [];
+    // for(let i = 0; i < str.length ; i++ ){
+    //     i > str.length-5 ? answer.push(str[i]) : answer.push('*')          
+    // }    
+    // return answer.join('');
+    return phone_number = [...phone_number].fill('*',0,phone_number.length-4).join('')
 }
