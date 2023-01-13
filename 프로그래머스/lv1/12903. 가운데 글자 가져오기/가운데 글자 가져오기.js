240a821d1e7a1e3cea4879f190e5070bb4eca3f2
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-13 23:23:36 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 2.43 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,5 +1,6 @@
 function solution(s) {
     
     console.log(Math.ceil(s.length/2))
-    return s.length % 2 === 0 ? s.slice(s.length/2-1,s.length/2+1) : s.slice(Math.ceil(~s.length/2),Math.ceil(s.length/2))
+    // return s.length % 2 === 0 ? s.slice(s.length/2-1,s.length/2+1) : s.slice(Math.ceil(~s.length/2),Math.ceil(s.length/2))
+    return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
 }
