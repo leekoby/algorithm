e32ad5362e0462aa32ddf3173e9401580747162f
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-14 17:12:18 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,18 @@
 function solution(n) {
-    let strN = String(n).split('');
-     console.log(strN)
-    let answer = 0;
-    for(let i =0 ; i<strN.length;i++){
-        answer += Number(strN[i])
-        console.log(strN[i])
-    }
-    return answer;
+    // let strN = String(n).split('');
+    //  console.log(strN)
+    // let answer = 0;
+    // for(let i =0 ; i<strN.length;i++){
+    //     answer += Number(strN[i])
+    //     console.log(strN[i])
+    // }
+    // return answer;
+    
+    
+    
+    return [...String(n)].map(Number).reduce((a,b)=> a+b);
+    // return Array.from(String(n)).reduce((a,b) => (+a) + (+b));
+    // return [...String(n)].reduce((a,b) => +a + +b);
+    
+    
 }
