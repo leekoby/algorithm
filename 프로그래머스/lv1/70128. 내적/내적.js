2e2bd208a99a8f7afa1074a895608809ca704f58
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-14 09:23:02 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,8 +1,9 @@
 function solution(a, b) {
-    var answer = 0;
+
     
-    for(let i = 0 ; i< a.length ; i ++){
-        answer += a[i] * b[i]
-        }
+    // for(let i = 0 ; i< a.length ; i ++){
+    //     answer += a[i] * b[i]
+    //     }
+     var answer = a.reduce((acc, cur, idx) => acc += cur*b[idx], 0)
     return answer;
 }
