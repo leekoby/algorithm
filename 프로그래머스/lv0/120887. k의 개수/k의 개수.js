98b1cfec4a11dd090cd9043091c8806c54726c31
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-16 08:16:52 +0900

    [level 0] Title: k의 개수, Time: 6.11 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -4,8 +4,7 @@ function solution(i, j, k) {
     for( let n = i ; n <=j ; n++){
             str+= n
         }
-    arr= str.split('')
-    arr.filter(el => +el === k ? answer++ : '')
-    console.log(arr, k)
-    return answer;
+  
+
+    return str.split(k).length-1;
 }
