d066169fdd92dc983b1b466009afd46a09c6e2de
leekoby <abc2752@naver.com>
2023-01-16 09:19:26 +0900

    dailycoding

---
+++ DailyCoding/01_transformFirstAndLast.js
@@ -0,0 +1,53 @@
+/*
+transformFirstAndLast
+문제
+배열을 입력받아 차례대로 배열의 첫 요소와 마지막 요소를 키와 값으로 하는 객체를 리턴해야 합니다.
+
+입력
+인자 1 : arr
+string 타입을 요소로 갖는 배열
+출력
+객체를 리턴해야 합니다.
+주의 사항
+빈 배열을 입력받은 경우, 빈 객체를 리턴해야 합니다.
+입력으로 받는 배열을 수정하지 않아야 합니다.
+입출력 예시
+1
+2
+3
+4
+5
+6
+7```js
+let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+let output = transformFirstAndLast(arr);
+console.log(output); // --> { Queen : 'Beyonce' }
+
+arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
+output = transformFirstAndLast(arr);
+console.log(output); // --> { Kevin : 'Spacey' }
+```
+*/
+
+
+
+
+function transformFirstAndLast(arr) {
+    let obj = {};
+    arr.length > 0 ? obj[arr[0]] = arr[arr.length - 1] : {}
+    return obj
+}
+
+
+
+
+
+
+
+let arr = ['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce'];
+let output = transformFirstAndLast(arr);
+console.log(output); // --> { Queen : 'Beyonce' }
+
+arr = ['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
+output = transformFirstAndLast(arr);
+console.log(output); // --> { Kevin : 'Spacey' }
---
+++ DailyCoding/02_computeWhenDouble.js
@@ -0,0 +1,44 @@
+/*
+computeWhenDouble
+문제
+연이율을 입력받아 원금이 2배 이상이 될 때까지 걸리는 시간(년)을 리턴해야 합니다.
+
+입력
+인자 1 : interestRate
+number 타입의 연이율 (%)
+출력
+number 타입을 리턴해야 합니다.
+입출력 예시
+1
+2
+3
+4
+5
+let output = computeWhenDouble(7);
+console.log(output); // --> 11
+
+output = computeWhenDouble(10);
+console.log(output); // --> 8
+*/
+
+function computeWhenDouble(interestRate) {
+    // TODO: 여기에 코드를 작성합니다.
+    let original = 1;  // 원금 
+    let year = 0;  // 
+
+    while (original < 2) { // 원금이 2배 이상이 될 때까지 반복 
+        // original = original * (1 + interestRate/100); // 계산식1 
+        original = original + (original * (interestRate / 100)); //계산식 2 
+        year++ // 
+
+    }
+
+    return year
+}
+
+
+let output = computeWhenDouble(7);
+console.log(output); // --> 11
+
+output = computeWhenDouble(10);
+console.log(output); // --> 8
