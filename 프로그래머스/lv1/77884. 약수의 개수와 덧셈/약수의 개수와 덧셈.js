d150e232fe59b5fd023bac874ae40140c73fabc2
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-17 19:38:12 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,27 +1,14 @@
-
-function checker (num){   
-        let cnt= 0;
-        for( let j = 1 ; j <= num; j ++){
-             if(num % j === 0  ){   
-                    cnt ++ 
-                
-            }
-        }
-               console.log(cnt)
-               return cnt 
-}
 function solution(left, right) {
-    var answer = 0;
-    
-    
-    
-    for(let i = left ; i<= right ; i++){
-        let chk = checker(i);
-        console.log(chk)
-            if(chk%2 === 0){
-                answer += i
-            }else if(chk %2 !== 0) answer -= i
-        }
-    
-    return answer; 
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
+    }
+    if (count % 2) answer -= i;
+    else answer += i;
+  }
+
+  return answer;
 }
