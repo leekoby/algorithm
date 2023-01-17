554528b42c94a1086d027299a1affe44c87dc8e6
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-17 19:50:39 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -1,10 +1,8 @@
 //3,6  4 5 6 , 7 8 <9
 
 function solution(sides) {
-    a = sides[0] < sides[1] ? sides[1]-sides[0]:sides[0]-sides[1] 
-    b = sides[0]+ sides[1]
-    console.log(a, b, b-a)
+  
     
     
-    return b-a-1
+    return Math.min(...sides)*2-1
     }
