2fc931b75b205f3beefde63c1a54bf73e67c35f9
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-17 19:58:43 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,5 +1,10 @@
 function solution(babbling) {
-        return babbling
-          .map((item) => item.replace(/aya|ye|woo|ma/g, ''))
-          .filter((str) => !str).length;
-      }
+  var answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach(word => {
+    if (regex.test(word)) answer++;  
+  })
+
+  return answer;
+}
