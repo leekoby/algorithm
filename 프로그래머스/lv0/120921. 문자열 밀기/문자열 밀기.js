b32c71b21e795407e1f9ae1c351938d822daf2f7
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-18 08:31:54 +0900

    [level 0] Title: 문자열 밀기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,13 +1,1 @@
-function solution(A, B) {
-    let arr = [...A]
-    for(let i = 0; i< arr.length; i++){
-        if(A === B)
-            return i;
-        else{
-            arr.unshift(arr.pop())
-            if(arr.join('') === B)
-                return i+1;
-        }
-    }
-    return -1;
-}
+let solution=(a,b)=>(b+b).indexOf(a)
