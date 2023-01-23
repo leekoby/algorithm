b02f3efd176f0efa69e0b75b0a62128c4343c196
leekoby <abc2752@naver.com>
2023-01-23 12:22:12 +0900

    1

--- test.js
+++ test.js
@@ -1,27 +1,39 @@
-const fs = require("fs");
-const { arrayBuffer } = require("stream/consumers");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath)
-    .toString()
-    .trim()
-    .split('\n')
-    .map(el => Number(el));
+// const readline = require('readline');
 
-let answer = input;
-let sum = answer.reduce((a, b) => a + b, 0)
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
-endOfCircuit: for (let i = 0; i < 8; i++) {
-    for (let j = i + 1; j < 9; j++) {
-        if ((sum - answer[i] - answer[j] === 100)) {
-            answer.splice(j, 1)
-            answer.splice(i, 1)
-            break endOfCircuit;
-        }
-    }
-}
-console.log(answer.sort((a, b) => a - b).join('\n'));
+// rl.on('line', function (line) {
+//     console.log(line);
+
+//     rl.close();
+// }).on("close", function () {
+//     process.exit();
+// });
+
+//===================================================================
+
+// const fs = require("fs");
+// const { arrayBuffer } = require("stream/consumers");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath)
+//     .toString()
+//     .trim()
+//     .split(' ')
+//     .map(el => el);
+// console.log(input)
+
+//===================================================================
+
+
+let answer;
+
+answer = Input.filter((v, i) => {
+    if (Input.indexOf(v) === i) { return true }
+})
 
 
 
-
-
+console.log(answer.join(' '));
