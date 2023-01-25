54be4505a108238a8f44f485836320fa0b7594f1
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-25 22:23:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.91 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,4 @@
 function solution(arr)
 {
-    let answer = [];
-    for (let i = 1; i <= arr.length ; i++ ) {
-            if ( arr[i] !== arr[i-1]) {
-               answer.push(arr[i-1]) 
-            }
-    }
-    // answer.push(arr[arr.length -1])
-    
-    return answer;     
+   return arr.filter((a,i) => a !== arr[i+1]);    
 }
