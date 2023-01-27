2389869e4956e9eebd3a05d1aa9e7602b5e5b0d9
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-27 13:27:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,9 @@
+function solution(common) {
+  if (common[2] - common[1] === common[1] - common[0]) {
+  
+    return common.pop() + common[1] - common[0];
+  } else {
+  
+    return common.pop() * (common[1] / common[0]);
+  }
+}
