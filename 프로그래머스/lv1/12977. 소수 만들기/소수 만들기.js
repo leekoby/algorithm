23bcd08c68fe95fd6c162e4e1cb4bf315816cd89
leekoby <118284808+leekoby@users.noreply.github.com>
2023-01-29 14:46:35 +0900

    [level 1] Title: 소수 만들기, Time: 2.88 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,7 +1,7 @@
 function isPrime(a){
     if(a < 2) return false;
     for(let i=2; i<= Math.sqrt(a) ;i++) 
-        if(a % i == 0) return false;
+        if(a % i === 0) return false;
     return true;
 }
 
@@ -10,8 +10,8 @@ function solution(nums) {
     let newNum = []
     var answer = 0;
     
-    for(let i = 0 ; i < nums.length ; i ++){
-        for ( let j = i+1 ; j < nums.length  ; j++){
+    for(let i = 0 ; i < nums.length-2 ; i ++){
+        for ( let j = i+1 ; j < nums.length-1  ; j++){
             for (let k = j + 1; k <nums.length ; k++){
                 newNum.push(nums[i]+nums[j]+nums[k])
                
