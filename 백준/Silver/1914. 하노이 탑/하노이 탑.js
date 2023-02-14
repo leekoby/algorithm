b76d17a843dd812272c58089828196b7d1bd3632
leekoby <118284808+leekoby@users.noreply.github.com>
2023-02-15 03:24:45 +0900

    [Silver I] Title: 하노이 탑, Time: 588 ms, Memory: 157732 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.js
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.js
@@ -1,32 +1,18 @@
-const readline = require("readline");
+const fs = require("fs");
+const input = Number(fs.readFileSync("/dev/stdin").toString());
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const answer = [];
 
-const moves = [];
-
-function hanoi(num, from, to, other) {
-  if (num === 1) {
-    moves.push(`${from} ${to}`);
-  } else {
-    hanoi(num - 1, from, other, to);
-    moves.push(`${from} ${to}`);
-    hanoi(num - 1, other, to, from);
-  }
+function hanoi(num, start, other, target) {
+  if (num === 0) return;
+    
+   hanoi(num - 1, start, target, other);
+   answer.push(`${start} ${target}`);
+   hanoi(num - 1, other, start, target);
 }
+console.log((BigInt(2 ** input) + BigInt(-1)).toString());
 
-rl.on("line", (input) => {
-  const n = BigInt(input);
-  const count = 2n ** n - 1n;
-  console.log(count.toString());
-
-  if (n <= 20n) {
-    hanoi(Number(n), 1, 3, 2);
-  }
-});
-
-rl.on("close", () => {
-  console.log(moves.join("\n"));
-});
+if (input <= 20) {
+    hanoi(input, 1, 2, 3);
+    console.log(answer.join("\n"));
+}
