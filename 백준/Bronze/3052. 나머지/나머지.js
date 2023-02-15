4bc4bac393c5917d69e051bf2e201af33c7aaeae
leekoby <118284808+leekoby@users.noreply.github.com>
2023-02-16 03:01:58 +0900

    [Bronze II] Title: 나머지, Time: 136 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+//* 입력 첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+// console.log(input);
+
+
+
+console.log(new Set(input.map(el => +el % 42)).size);
