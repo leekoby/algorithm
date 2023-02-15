f67c246b0381a22a8afe4951bfeab8fcbae0c61f
leekoby <abc2752@naver.com>
2023-02-16 01:20:07 +0900

    Update 베스트앨범.js

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -4,22 +4,22 @@
 //2개씩 묶는다
 
 function solution(genres, plays) {
-   const genreMap = new Map();
+    const genreMap = new Map();
     genres
-        .map((genre,index)=> [genre,plays[index]] )
-        .forEach(([genre,play],index)=>{
-            const data = genreMap.get(genre) || {total:0,songs:[]};
-            genreMap.set(genre,{
-                total:data.total + play,
-                songs: [...data.songs, {play, index}]
-                    .sort((a,b) => b.play-a.play)
-                    .slice(0,2)
+        .map((genre, index) => [genre, plays[index]])
+        .forEach(([genre, play], index) => {
+            const data = genreMap.get(genre) || { total: 0, songs: [] };
+            genreMap.set(genre, {
+                total: data.total + play,
+                songs: [...data.songs, { play, index }]
+                    .sort((a, b) => b.play - a.play)
+                    .slice(0, 2)
             })
-        
-    })
+
+        })
     return [...genreMap.entries()]
-            .sort((a,b)=>b[1].total - a[1].total)
-            .flatMap(item=>item[1].songs)
-            .map(song=>song.index)
-    
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap(item => item[1].songs)
+        .map(song => song.index)
+
 }
