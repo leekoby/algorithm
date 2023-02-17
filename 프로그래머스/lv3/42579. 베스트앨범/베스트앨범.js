b6ab6394319ea6dd5b99e2a04ca0af4aa7bb48b3
leekoby <118284808+leekoby@users.noreply.github.com>
2023-02-17 21:47:40 +0900

    [level 3] Title: 베스트앨범, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -4,22 +4,26 @@
 //2개씩 묶는다
 
 function solution(genres, plays) {
-    const genreMap = new Map();
-    genres
-        .map((genre, index) => [genre, plays[index]])
-        .forEach(([genre, play], index) => {
-            const data = genreMap.get(genre) || { total: 0, songs: [] };
-            genreMap.set(genre, {
-                total: data.total + play,
-                songs: [...data.songs, { play, index }]
-                    .sort((a, b) => b.play - a.play)
-                    .slice(0, 2)
-            })
-
-        })
-    return [...genreMap.entries()]
-        .sort((a, b) => b[1].total - a[1].total)
-        .flatMap(item => item[1].songs)
-        .map(song => song.index)
-
+  // 장르별 총 재생 수와 2개의 높은 재생 수를 가진 노래를 저장하는 Map() 객체 생성
+  const genreMap = new Map();
+  
+  // genres와 plays 배열을 순회하며 Map() 객체에 데이터를 추가
+  genres.map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      // Map() 객체에서 해당 장르의 정보를 가져옴, 없는 경우 초기화
+      const data = genreMap.get(genre) || { total: 0, songs: [] };
+      // 해당 장르의 총 재생 수와 노래 정보를 갱신
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a, b) => b.play - a.play) // 노래를 재생 수가 높은 순으로 정렬
+          .slice(0, 2) // 재생 수가 높은 2개의 노래만 선택
+      })
+    })
+  
+  // Map() 객체를 배열로 변환하여 장르별 총 재생 수로 정렬한 후 노래 인덱스를 추출
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap(item => item[1].songs)
+    .map(song => song.index)
 }
