de96a77e84029ac12b9af86bff86f5aadadb4595
leekoby <abc2752@naver.com>
2023-02-20 22:50:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,12 @@
 function solution(n, arr1, arr2) {
   // 해시(hash) 생성
-  const hash = arr1.reduce((acc, cur, i) => {
+  const decodedMap = arr1.reduce((acc, cur, i) => {
     const combined = cur | arr2[i]; // 비트 OR 연산
-    acc.push(toBinary(combined, n)); // 2진수로 변환한 값을 배열에 추가
-    return acc;
+    const binary = toBinary(combined, n); // 2진수로 변환
+    const decodedLine = binary.replace(/1|0/g, (match) =>
+      match === '1' ? '#' : ' '
+    ); // 해독된 지도 한 줄
+    return [...acc, decodedLine];
   }, []);
 
   // 2진수로 변환하는 함수
@@ -14,11 +17,6 @@ function solution(n, arr1, arr2) {
     }
     return bin;
   }
-
-  // 비밀지도 해독
-  const decodedMap = hash.map((line) =>
-    line.replace(/1|0/g, (match) => (match === '1' ? '#' : ' '))
-  );
 
   return decodedMap;
 }
