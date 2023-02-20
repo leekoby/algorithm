7863612f138031fa4b7e384ba6b7ba3b7632e3db
leekoby <abc2752@naver.com>
2023-02-20 20:20:59 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,10 @@
 function solution(n, arr1, arr2) {
-  // 비트 OR 연산을 위한 Map 생성
-  const bitMap = new Map();
-  for (let i = 0; i < Math.pow(2, n); i++) {
-    bitMap.set(i, toBinary(i, n));
-  }
+  // 해시(hash) 생성
+  const hash = arr1.reduce((acc, cur, i) => {
+    const combined = cur | arr2[i]; // 비트 OR 연산
+    acc.push(toBinary(combined, n)); // 2진수로 변환한 값을 배열에 추가
+    return acc;
+  }, []);
 
   // 2진수로 변환하는 함수
   function toBinary(num, len) {
@@ -14,15 +15,8 @@ function solution(n, arr1, arr2) {
     return bin;
   }
 
-  // 두 배열을 합친 비밀지도 생성
-  const secretMap = arr1.map((num1, i) => {
-    const num2 = arr2[i];
-    const combined = num1 | num2; // 비트 OR 연산
-    return bitMap.get(combined);
-  });
-
   // 비밀지도 해독
-  const decodedMap = secretMap.map((line) =>
+  const decodedMap = hash.map((line) =>
     line.replace(/1|0/g, (match) => (match === '1' ? '#' : ' '))
   );
 
