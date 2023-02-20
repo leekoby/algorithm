35d2da5f1b8d51e403e33cfe8d43095d64627470
leekoby <abc2752@naver.com>
2023-02-21 02:05:37 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,16 +1,5 @@
 function solution(order) {
-    let str = String(order)
-    let newArr = str.split('')
-    let answer = 0;
-    
-    
-    for (let i = 0 ; i < newArr.length ; i++){
-        if(newArr[i] !== '0' && newArr[i]%3 ===0 )
-            answer++;
-        
-    }
-    
-    
-    
-    return answer;
+    return String(order).split('').filter(el => +el !== 0 && +el%3===0).length
+       
+  
 }
