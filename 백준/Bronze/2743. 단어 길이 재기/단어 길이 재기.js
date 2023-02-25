4567d3dda6aa5adb5b11cd42f33a6090f79d1809
leekoby <abc2752@naver.com>
2023-02-26 02:40:08 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 156 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let data = input[0]
+console.log(data.length)
