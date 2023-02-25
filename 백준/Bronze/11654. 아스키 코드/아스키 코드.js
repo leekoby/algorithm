a43bcb57f176c6f4659b23d93a10a47754066b3e
leekoby <abc2752@naver.com>
2023-02-26 02:37:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let ascii = input[0]
+console.log(ascii.charCodeAt(0))
+
