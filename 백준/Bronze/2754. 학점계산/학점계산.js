0368d9c4d422e3ef582ae58dec7ffdec7e26da4c
leekoby <abc2752@naver.com>
2023-03-03 19:33:02 +0900

    [Bronze V] Title: 학점계산, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim()
+
+
+
+let grade = {
+  'A+': '4.3',
+  'A0': '4.0',
+  'A-': '3.7',
+  'B+': '3.3',
+  'B0': '3.0',
+  'B-': '2.7',
+  'C+': '2.3',
+  'C0': '2.0',
+  'C-': '1.7',
+  'D+': '1.3',
+  'D0': '1.0',
+  'D-': '0.7',
+  'F': '0.0'
+}
+
+console.log(grade[`${input}`])
