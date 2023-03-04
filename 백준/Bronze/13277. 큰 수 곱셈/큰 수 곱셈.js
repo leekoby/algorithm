ae04fda1edcf5853fa8ad762caa4370969a1db64
leekoby <abc2752@naver.com>
2023-03-05 00:06:56 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 900 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [a, b] = fs.readFileSync(filePath).toString().trim().split(' ').map(BigInt)
+
+
+
+console.log((a * b).toString())
