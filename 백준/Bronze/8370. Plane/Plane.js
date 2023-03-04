10a0ab1dd4742f078e9b9dddabc0969a37ea02da
leekoby <abc2752@naver.com>
2023-03-05 00:14:04 +0900

    [Bronze V] Title: Plane, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [a, b, c, d] = fs.readFileSync(filePath).toString().trim().split(' ').map(Number)
+
+
+console.log(a * b + c * d)
