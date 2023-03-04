120508ecd191e36cf1e27b5e932e7c2f02f39092
leekoby <abc2752@naver.com>
2023-03-04 23:50:56 +0900

    [Bronze V] Title: 홍익대학교, Time: 120 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = +fs.readFileSync(filePath).toString().trim()
+// .split('\n').reduce((a, b) => +a + +b, 0)
+
+
+console.log(input - 1946)
