5b35e5061c345981918030540da421d819a5999e
leekoby <abc2752@naver.com>
2023-03-15 02:04:33 +0900

    Create 230315_중위순회.js

---
+++ Study/이진트리/230315_중위순회.js
@@ -0,0 +1,25 @@
+/*
+          1
+        /   \
+      2       3
+     / \     /  \
+   4    5    6    7
+*/
+
+function solution(v) {
+  let answer;
+  function recursion(v) {
+    if (v > 7) {
+      return;
+    } else {
+      recursion(v * 2);
+      console.log(v)
+      recursion(v * 2 + 1);
+
+    }
+  }
+  recursion(v);
+  return answer;
+}
+
+console.log(solution(1))
