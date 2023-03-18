3327e6153d41fc4efa048fbbfc8a7b0b27874d55
leekoby <abc2752@naver.com>
2023-03-18 13:35:34 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1400 ms, Memory: 180652 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -1,12 +1,14 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : `${__dirname}/input.txt`;
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n')
+
+let n = Number(input[0])
 
 let answer = '';
 
-for (let idx = 1; idx <= Number(input[0]); idx++) {
-  const inputs = input[idx].split(' ').map(Number);
-  answer = idx < Number(input[0]) ? answer + `${inputs[0] + inputs[1]}\n`
+for (let i = 1; i <= n; i++) {
+  const inputs = input[i].split(' ').map(Number);
+  answer = i < Number(input[0]) ? answer + `${inputs[0] + inputs[1]}\n`
     : answer + `${inputs[0] + inputs[1]}`;
 }
 
