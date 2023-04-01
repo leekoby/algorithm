8fded95ab24ee5acd3e0f4c738d2b97b5519451e
이호승 <abc2752@naver.com>
2023-04-01 16:31:33 +0900

    Time: 66 ms (26.66%), Space: 41.4 MB (85.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 const climbStairs = function(n) {
-  if (n <= 2) {
-    return n;
-  }
+    const d = new Array(n + 1).fill(0);
+    d[1] = 1;
+    d[2] = 2;
 
-  const d = new Array(n + 1).fill(0);
-  d[1] = 1;
-  d[2] = 2;
+    function dp(x) {
+        if (d[x] !== 0) {
+            return d[x];
+        }
+        d[x] = dp(x - 1) + dp(x - 2);
+        return d[x];
+    }
 
-  for (let i = 3; i <= n; i++) {
-    d[i] = d[i - 1] + d[i - 2];
-  }
-
-  return d[n];
+    return dp(n);
 };
 
 
