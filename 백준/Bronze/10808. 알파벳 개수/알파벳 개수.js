a0f995529c0674f8b0db2335fe7cc009e08d6ca5
이호승 <abc2752@naver.com>
2023-04-02 17:06:31 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let n = fs.readFileSync(filePath).toString().trim()
+
+const alpha = {
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  e: 0,
+  f: 0,
+  g: 0,
+  h: 0,
+  i: 0,
+  j: 0,
+  k: 0,
+  l: 0,
+  m: 0,
+  n: 0,
+  o: 0,
+  p: 0,
+  q: 0,
+  r: 0,
+  s: 0,
+  t: 0,
+  u: 0,
+  v: 0,
+  w: 0,
+  x: 0,
+  y: 0,
+  z: 0,
+}
+for (const x of n) alpha[x]++
+answer = "";
+for (const x in alpha) answer += alpha[x] + " "
+console.log(answer)
