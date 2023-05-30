e714e5f4bdb1a1541b2e2b41747e1bc1db9c3a6f
이호승 <abc2752@naver.com>
2023-05-30 23:20:11 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers, n) {
+    let answer=0
+    for(let i = 0 ; i < numbers.length ; i++ ){
+        answer+=numbers[i]
+          if(answer > n) break
+    }
+    
+    return answer
+}
