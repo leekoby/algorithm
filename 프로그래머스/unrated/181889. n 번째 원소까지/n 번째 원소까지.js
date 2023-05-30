51676c73383c4866b40aed0dcc6b29e22b2fe831
이호승 <abc2752@naver.com>
2023-05-31 00:09:37 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,7 +1,1 @@
-function solution(num_list, n) {
-    let answer = [];
-    for(let i = 0; i < n ; i ++){
-        answer.push(num_list[i])
-    }
-    return answer;
-}
+const solution = (l,n)=> l.splice(0,n)
