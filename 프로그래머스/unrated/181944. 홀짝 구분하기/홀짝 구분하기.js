bd4cee5dd0f35ad4f383bb0e180c2a7ece42720d
이호승 <abc2752@naver.com>
2023-06-06 12:36:25 +0900

    [unrated] Title: 홀짝 구분하기, Time: 47.41 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,20 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    let ans=''
+    
+    
+    ans=n%2===0 ? `${n} is even`:`${n} is odd`
+    
+    console.log(ans)
+    
+});
