11dd05a7f05ea2343941c1b610e9ea57d6d06904
이호승 <abc2752@naver.com>
2023-06-09 19:44:57 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_string, index_list) {
+    var answer = '';
+    for(let i = 0; i< index_list.length;i++){
+        answer +=  my_string[index_list[i]]
+    }
+    return answer;
+    
+}
