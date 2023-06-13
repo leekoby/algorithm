9aae23d4c762750baaf50e526c93585d38d733d4
이호승 <abc2752@naver.com>
2023-06-13 11:10:11 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,14 +1,10 @@
 function solution(arr) {
-  let isValid = true 
+ let len = arr.length
   
-  for(let i = 0 ; i < arr.length;i++){
-      for(let j = 0 ; j< arr[i].length;j++){
-          
-          if(!(arr[i][j]===arr[j][i])) {
-              isValid =false
-              return 0 
-          }
+  for(let i = 0 ; i < len;i++){
+      for(let j = 0 ; j< len;j++){
+        if(!(arr[i][j]===arr[j][i])) return 0           
       }
   }
-    return +isValid
+    return 1
 }
