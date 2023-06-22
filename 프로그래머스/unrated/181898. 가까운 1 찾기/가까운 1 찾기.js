50bc439121fe1f91d2e1caa4f09f8f796430f9a7
이호승 <abc2752@naver.com>
2023-06-22 23:48:42 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,7 +1,1 @@
-function solution(arr, idx) {
-    for(let i = idx ; i < arr.length; i++) {
-        const cur = arr[i]
-        if(cur === 1) return i
-    }
-    return -1
-}
+const solution=(a,i)=>a.indexOf(1,i);
