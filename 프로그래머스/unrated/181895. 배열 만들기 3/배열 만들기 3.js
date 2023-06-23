5f2c81b165ceb28d9f939acae3720e7338dfee31
이호승 <abc2752@naver.com>
2023-06-24 01:49:51 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.45 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,7 +1,5 @@
 function solution(arr, intervals) {
-    var answer = [];
-    
-    intervals.map(v=> answer.push(...arr.slice(v[0],v[1]+1)))
-    
-    return answer;
+    const [[a,b],[c,d]] = intervals;
+
+    return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
 }
