0cb89bbc42fb21e74c93dd9141a5087b6c9879dc
LeeHoSeung <abc2752@naver.com>
2023-07-10 00:33:46 +0900

    [unrated] Title: 세로 읽기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,3 @@
 function solution(str, m, c) {
-    let ans=''
-    for(let i = c-1 ; i < str.length; i=i+m){
-        ans+=str[i]
-    }
-    
-    
-    
-    return ans
+    return [...str].filter((_,i)=> i%m ===c-1).join('')
 }
