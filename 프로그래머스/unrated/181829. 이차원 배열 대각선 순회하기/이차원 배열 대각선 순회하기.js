69185e55000e22ca2cce9321eb316db5c1ad2afc
LeeHoSeung <abc2752@naver.com>
2023-07-16 00:09:12 +0900

    [unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.js
@@ -0,0 +1,10 @@
+function solution(board, k) {
+    let sum= 0
+    for(let i = 0 ; i < board.length ; i ++) {
+        for(let j = 0 ; j < board[i].length ; j ++) {
+            if(i+j <= k) sum+= board[i][j]
+        }
+    }
+    
+    return sum
+}
