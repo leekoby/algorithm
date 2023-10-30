57709ac0f7bee33b179f096943a9ef5f6d956fbc
LeeHoSeung <abc2752@naver.com>
2023-10-31 06:28:16 +0900

    [level unrated] Title: 양꼬치, Time: 0.01 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int n, int k) {
-        int service = n/10;
-        int answer = n*12000 + k*2000- service*2000;
-        return answer;
+      return n*12000 + k*2000- (n/10)*2000;
     }
 }
